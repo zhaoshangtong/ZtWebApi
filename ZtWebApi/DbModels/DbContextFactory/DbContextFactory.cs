94b6d1d179b383aa3ce848693f3c3e8ad01003cd
e <zhaoshangtong@163.com>
2018-06-22 18:32:26 +0800

    add

---
+++ ZtWebApi/DbModels/DbContextFactory/DbContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbModels.DbContextFactory
+{
+    public class DbContextFactory
+    {
+        /// <summary>
+        /// 获取当前EF数据上下文
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static DbContext GetCurrentDbContext(string key=null)
+        {
+            DbContext nContext = CallContext.GetData(key + "DBContext") as DbContext;
+            if (nContext == null)
+            {
+                nContext = GetContextByKey(key);
+                CallContext.SetData(key + "DBContext", nContext);
+            }
+            return nContext;
+        }
+
+        internal static DbContext GetContextByKey(string key)
+        {
+            DbContext db = new EFMssqlCodeFirst.EFCodeFirst();
+            if (key == "EFMysql")
+            {
+                db = new EFMysql.EFMysql();
+            }
+            return db;
+        }
+    }
+}
